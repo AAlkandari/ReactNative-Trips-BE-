75e5280650b89de4fd9a37c9b5c48e827bd3d859
AAlkandari <96235631+AAlkandari@users.noreply.github.com>
2022-02-20 11:42:44 +0300

    Done from SignIn & SignUp

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
-import express from "express";
-
 const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+
+// Required Routes
+const userRoutes = require("./apis/users/users.routes");
+
+// Database Connection
+const connectDB = require("./db/database");
+
+// Required Middlewares
+const logger = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+
+// Required Passport
+const { localStrategy, jwtStrategy } = require("./middleware/passport");
 
 const app = express();
+connectDB();
 
-app.listen(8000, () => {
-  console.log("The application is running on localhost:8000");
-});
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(logger);
+app.use(errorHandler);
+
+// Passport
+app.use(passport.initialize());
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+// Routes
+app.use("/api", userRoutes);
+
+const PORT = 8000;
+app.listen(PORT, () => console.log(`Application running on localhost:${PORT}`));
