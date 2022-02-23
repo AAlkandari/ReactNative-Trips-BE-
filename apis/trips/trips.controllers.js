5b5a9d6542636ff1e62f181b7f22a81ab1af0212
Your Name <mohammadalhamdan94@gmail.com>
2022-02-23 10:13:26 +0300

    mnmb

--- apis/trips/trips.controllers.js
+++ apis/trips/trips.controllers.js
@@ -34,8 +34,10 @@ exports.tripCreate = async (req, res, next) => {
 
 exports.tripDelete = async (req, res, next) => {
   try {
-    await req.trip.remove();
-    res.status(204).end();
+    const deleteTrip = await Trip.findByIdAndDelete({
+      _id: req.trip.id,
+    }).populate("owner");
+    res.json(deleteTrip);
   } catch (error) {
     next(error);
   }
@@ -46,10 +48,14 @@ exports.tripUpdate = async (req, res, next) => {
       req.body.image = `${req.protocol}://${req.get("host")}/${req.file.path}`;
     }
     console.log(req.body); //new:true to to show the update after change immiditly
-    const trip = await Trip.findByIdAndUpdate({ _id: req.trip.id }, req.body, {
-      new: true,
-      runValidators: true,
-    });
+    const trip = await Trip.findByIdAndUpdate(
+      { _id: req.trip.id, owner: req.trip.owner },
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
 
     res.json(trip);
   } catch (err) {
